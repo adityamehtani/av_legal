350c44be07a45803d45b23976decb2b5d516d7d4
adityamehtani <66093430+adityamehtani@users.noreply.github.com>
2025-07-26 17:07:23 +0530

    added homepage index.vue

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div style="padding: 2rem;">
+    <h1>👋 Welcome to AV LEGAL AI Dashboard</h1>
+    <ul>
+      <li><a href="/cases">Go to Case Tracker</a></li>
+      <li><a href="/summarize">Go to Summarizer</a></li>
+    </ul>
+  </div>
+</template>
